0eb52a66f313fa00e742401587f4ddc9d5ea39ec
bhaveek424 <jainbhaveek.v727@gmail.com>
2022-12-10 19:28:39 +0530

    mongodb connection

--- server/express.js
+++ server/express.js
@@ -1,7 +1,14 @@
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
 import express from "express";
 
 const app = express();
 
 /*.....configure express..... */
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(helmet());
+app.use(cors());
 
 export default app;
