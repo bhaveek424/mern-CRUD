42a5ffbdd657187126550e3e138d6b198dacd33c
bhaveek424 <jainbhaveek.v727@gmail.com>
2022-12-11 10:43:39 +0530

    Auth API: signin

--- server/express.js
+++ server/express.js
@@ -10,10 +10,13 @@ import authRoutes from "./routes/auth.routes";
 const app = express();
 
 /*.....configure express..... */
+// parse body params and attach them to req.body
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
+// secure apps by setting various HTTP headers
 app.use(helmet());
+// enable CORS - Cross Origin Resource Sharing
 app.use(cors());
 
 //mount routes
