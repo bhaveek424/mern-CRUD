6ed553af8cbc98919334a0cfcf56ec76ad384166
bhaveek424 <jainbhaveek.v727@gmail.com>
2022-12-11 09:54:54 +0530

    API Endpoint: Deleting

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -2,70 +2,83 @@
 controller methods that were used in the preceding user route declarations as
 callbacks to be executed when a route request is received by the server. */
 import User from "../models/user.model";
-import extend from 'loadash/extend'
-import errorHandler from '../helpers/dbErrorHandler'
+import extend from "loadash/extend";
+import errorHandler from "../helpers/dbErrorHandler";
 
 const create = async (req, res) => {
-  const user = new User(req.body)
-  try{
-    await user.save()
+  const user = new User(req.body);
+  try {
+    await user.save();
     return res.status(200).json({
-      messgae: "Successfully signed up! "
-    })
-  } catch(err) {
+      messgae: "Successfully signed up! ",
+    });
+  } catch (err) {
     return res.status(400).json({
-      error: errorHandler.getErrorMessage
-    })
+      error: errorHandler.getErrorMessage,
+    });
   }
-}
+};
 const list = async (req, res) => {
   try {
-    let users = await User.find().select('name email updated created') 
-    res.json(users)
-  } catch(err) {
+    let users = await User.find().select("name email updated created");
+    res.json(users);
+  } catch (err) {
     return res.status(400).json({
-      error: errorHandler.getErrorMessage(err)
-    })
+      error: errorHandler.getErrorMessage(err),
+    });
   }
-}
+};
 /*
-* Load user and append to req
+ * Load user and append to req
  */
 const userById = async (req, res, next, id) => {
   try {
-    let user = await User.findById(id)
-    if(!user) return res.status('400').json({
-      error: "User not found!"
-    })
-    req.profile = user
-    next()
-  } catch(err) {
-    return res.status('400').json({
-      error: "Could not retrieve user"
-    })
+    let user = await User.findById(id);
+    if (!user)
+      return res.status("400").json({
+        error: "User not found!",
+      });
+    req.profile = user;
+    next();
+  } catch (err) {
+    return res.status("400").json({
+      error: "Could not retrieve user",
+    });
   }
-}
+};
 
 const read = (req, res) => {
-  req.profile.hashed_password = undefined
-  req.profile.salt = undefined
-  return res.json(req.profile)
-}
+  req.profile.hashed_password = undefined;
+  req.profile.salt = undefined;
+  return res.json(req.profile);
+};
 const update = async (req, res) => {
   try {
-    let user = req.profile
-    user = extend(user, req.body)
-    user.updated = Date.now()
-    await user.save()
-    user.hashed_password = undefined
-    user.salt = undefined
-    res.json(user)
-  } catch(err) {
+    let user = req.profile;
+    user = extend(user, req.body);
+    user.updated = Date.now();
+    await user.save();
+    user.hashed_password = undefined;
+    user.salt = undefined;
+    res.json(user);
+  } catch (err) {
     return res.status(400).json({
-      error: errorHandler.getErrorMessage(err)
-    })
+      error: errorHandler.getErrorMessage(err),
+    });
   }
-}
-const remove = (req, res, next) = {}
+};
+const remove = async (req, res) => {
+  try {
+    let user = req.profile;
+    let deletedUser = await user.remove();
+    deletedUser.hashed_password = undefined;
+    deletedUser.salt = undefined;
+    res.json(deletedUser);
+  } catch (err) {
+    return res.status(400).json({
+      error: errorHandler.getErrorMessage(err),
+    });
+  }
+};
 
-export default {create, list, userById, read, update, remove}
+export default { create, list, userById, read, update, remove };
