4c550580d9394d496a272d6dc371d61e4303c761
bhaveek424 <jainbhaveek.v727@gmail.com>
2022-12-11 09:41:54 +0530

    API Endpoint: Loading user

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -28,7 +28,23 @@ const list = async (req, res) => {
     })
   }
 }
-const userById = (req, res, next, id) => {}
+/*
+* Load user and append to req
+ */
+const userById = async (req, res, next, id) => {
+  try {
+    let user = await User.findById(id)
+    if(!user) return res.status('400').json({
+      error: "User not found!"
+    })
+    req.profile = user
+    next()
+  } catch(err) {
+    return res.status('400').json({
+      error: "Could not retrieve user"
+    })
+  }
+}
 const read = (req, res)= {}
 const update = (req, res, next)= {}
 const remove = (req, res, next) = {}
